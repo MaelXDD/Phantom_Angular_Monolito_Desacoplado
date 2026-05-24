ffa13de7a857001c6865875b500fa1723dc1af6b
MaelXD <fiestasjheins@gmail.com>
2026-05-23 21:47:25 -0500

    Primer comit

--- phantom-app/src/app/auth/login/login.ts
+++ phantom-app/src/app/auth/login/login.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.spec';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  enviarLogin() {
+    this.authService.login({ email: this.email, password: this.password }).subscribe({
+      next: (res) => {
+        // Si el login es exitoso, Java nos devuelve el rol
+        if (res.rol === 'ADMIN') {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/inicio']);
+        }
+      },
+      error: (err) => alert('Credenciales incorrectas'),
+    });
+  }
+}
